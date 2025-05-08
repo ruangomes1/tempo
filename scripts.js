f63db9230a9ad59337d8a0d53ba0261532b5bc50
ruangomes1 <ruanalvesgomes26@gmail.com>
2025-05-07 22:07:15 -0300

    O-tempo

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+let chave = "33fe7f95b759f03eb13219ed996a893a"
+
+function colocarNaTela(dados){
+    console.log(dados)
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "ºC"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".umidade").innerHTML = "Umidade " + (dados.main.humidity) + "%"
+
+}    
+
+async function buscarCidade(cidade) {
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+        cidade +
+        "&appid=" +
+        chave +
+        "&lang=pt_br" +
+        "&units=metric"
+    )
+        .then(resposta => resposta.json())
+
+        colocarNaTela(dados)
+
+}
+
+
+function cliqueiNoBotao() {
+    let cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
